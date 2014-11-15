69061603b0878bf59ab04b28935e25baf91fb57a
jml <jml@debian>
2014-11-15 16:29:51 +0100

    from old PC.

---
+++ appsrc/vm/main.c
@@ -0,0 +1,21 @@
+/*
+ *
+ *                          09 March MMXII PUBLIC DOMAIN
+ *             The author disclaims copyright to this source code.
+ *
+ *          s = (a + b + c) / 2.0;
+ *          A = sqrt(s * (s - a) * (s - b) * (s - c));    // Heron of Alexandria
+ *
+ * 
+ */
+
+ 
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
